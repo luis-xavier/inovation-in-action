358db27a25f1ccf1376b74f66d3e3838fe0f09cc
luis-xavier <luis_xavier_x@yahoo.com.mx>
2026-05-01 13:30:46 -0600

    lowering versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     integrations: [],
     vite: {
-        plugins: [tailwind()]
+        plugins: [tailwindcss()]
     }
 });
